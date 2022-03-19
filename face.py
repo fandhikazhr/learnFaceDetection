70cb819ff8a9c0619f62f8c0382d301138cfbfa2
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-03-20 04:58:26 +0700

    Update face.py

--- face.py
+++ face.py
@@ -1,3 +1,8 @@
 import cv2
 import mediapipe as mp
 import time
+
+cap = cv2.VideoCapture(0)
+mpFaceDetection = mp.solutions.face_detection
+mpDraw = mp.solutions.drawing_utils
+faceDetection = mpFaceDetection.FaceDetection(0.75)
