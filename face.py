3d197437d1553c7870779bd6887846ce1ce3a352
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-03-20 04:58:46 +0700

    Update face.py

--- face.py
+++ face.py
@@ -6,3 +6,8 @@ cap = cv2.VideoCapture(0)
 mpFaceDetection = mp.solutions.face_detection
 mpDraw = mp.solutions.drawing_utils
 faceDetection = mpFaceDetection.FaceDetection(0.75)
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = faceDetection.process(imgRGB)
