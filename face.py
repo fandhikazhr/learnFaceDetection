4d41fa3b9fba1089219c2ea8a468b90f8b8642d0
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-03-20 04:59:28 +0700

    Update face.py

--- face.py
+++ face.py
@@ -14,3 +14,13 @@ while True:
 
     if results.detections:
         for id,detection in enumerate(results.detections):
+            mpDraw.draw_detection(img, detection)
+            bboxC = detection.location_data.relative_bounding_box
+            ih, iw, ic = img.shape
+            bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), \
+                   int(bboxC.width * iw), int(bboxC.height * ih)
+            cv2.rectangle(img, bbox, (80, 255, 0), 2)
+            cv2.putText(img, f'{int(detection.score[0]*100)}%',
+                        (bbox[0],bbox[1]-20),
+                        cv2.FONT_HERSHEY_PLAIN,
+                        3, (0, 255, 0), 2)
