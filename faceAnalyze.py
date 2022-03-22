d469f0ad6228870a21bc281d39b576b877ee99e4
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-03-22 23:44:43 +0700

    Update faceAnalyze.py

--- faceAnalyze.py
+++ faceAnalyze.py
@@ -1,3 +1,11 @@
 import cv2
 import mediapipe as mp
 import time
+
+cap = cv2.VideoCapture(0)
+pTime = 0
+
+mpDraw = mp.solutions.drawing_utils
+mpFaceMesh = mp.solutions.face_mesh
+faceMesh = mpFaceMesh.FaceMesh(max_num_faces=2)
+drawSpec = mpDraw.DrawingSpec((0,255,0),thickness=1, circle_radius=2)
