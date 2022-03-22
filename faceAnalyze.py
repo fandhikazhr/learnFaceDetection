4fd8d32466a3ce857834c4de3dae27bef93929eb
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-03-22 23:45:00 +0700

    Update faceAnalyze.py

--- faceAnalyze.py
+++ faceAnalyze.py
@@ -9,3 +9,17 @@ mpDraw = mp.solutions.drawing_utils
 mpFaceMesh = mp.solutions.face_mesh
 faceMesh = mpFaceMesh.FaceMesh(max_num_faces=2)
 drawSpec = mpDraw.DrawingSpec((0,255,0),thickness=1, circle_radius=2)
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = faceMesh.process(imgRGB)
+    if results.multi_face_landmarks:
+        for faceLM in results.multi_face_landmarks:
+            mpDraw.draw_landmarks(img, faceLM, mpFaceMesh.FACEMESH_CONTOURS,
+                                  drawSpec,drawSpec)
+            for id, lm in enumerate(faceLM.landmark):
+                # print(lm)
+                ih, iw, ic = img.shape
+                x, y = int(lm.x*iw), int(lm.y*ih)
+                print(id,x,y)
