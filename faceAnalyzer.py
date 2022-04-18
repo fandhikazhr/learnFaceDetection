053e00223e2485d2e9efd3ac8c64285f8fb2a878
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-04-18 19:31:35 +0700

    Update faceAnalyzer.py

--- faceAnalyzer.py
+++ faceAnalyzer.py
@@ -1,3 +1,13 @@
 import cv2
 import mediapipe as mp
 import time
+
+class FaceDetector():
+    def __init__(self, minDetectionCon= 0.5):
+
+        self.minDetectionCon = minDetectionCon
+
+        self.mpFaceDetection = mp.solutions.face_detection
+        self.mpDraw = mp.solutions.drawing_utils
+        # self.faceDetection = self.mpFaceDetection.FaceDetection(0.75)
+        self.faceDetection = self.mpFaceDetection.FaceDetection(self.minDetectionCon)
