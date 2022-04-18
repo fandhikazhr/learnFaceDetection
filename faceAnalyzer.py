7515810c7b900b70aa201f18b745d0e20f551b1b
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-04-18 19:32:03 +0700

    Update faceAnalyzer.py

--- faceAnalyzer.py
+++ faceAnalyzer.py
@@ -11,3 +11,10 @@ class FaceDetector():
         self.mpDraw = mp.solutions.drawing_utils
         # self.faceDetection = self.mpFaceDetection.FaceDetection(0.75)
         self.faceDetection = self.mpFaceDetection.FaceDetection(self.minDetectionCon)
+    
+    def findFaces(self, img, draw = True):
+
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.faceDetection.process(imgRGB)
+        # print(self.results)
+        bboxs = []
