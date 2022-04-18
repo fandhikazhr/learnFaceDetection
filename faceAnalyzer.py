0c52af178cd7630baa7ad083d4cc57929d28b249
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-04-18 19:33:14 +0700

    Update faceAnalyzer.py

--- faceAnalyzer.py
+++ faceAnalyzer.py
@@ -26,3 +26,11 @@ class FaceDetector():
                 bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), \
                        int(bboxC.width * iw), int(bboxC.height * ih)
                 bboxs.append([id, bbox, detection.score])
+                if draw:
+                    img = self.fancyDraw(img, bbox)
+                    # cv2.rectangle(img, bbox, (80, 255, 0), 2)
+                    cv2.putText(img, f'{int(detection.score[0]*100)}%',
+                            (bbox[0],bbox[1]-20),
+                            cv2.FONT_HERSHEY_PLAIN,
+                            3, (0, 255, 0), 2)
+        return img, bboxs
