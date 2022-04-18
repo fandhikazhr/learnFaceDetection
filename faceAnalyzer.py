3c53fac4b04bcb8846f7d0d5c0c47496109891b6
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-04-18 19:34:52 +0700

    Update faceAnalyzer.py

--- faceAnalyzer.py
+++ faceAnalyzer.py
@@ -54,3 +54,22 @@ class FaceDetector():
         cv2.line(img, (x1, y1), (x1, y1 - l), (80, 255, 0), thickness)
 
         return img
+
+def main ():
+    cap = cv2.VideoCapture(0)
+    pTime = 0
+    detector = FaceDetector()
+    while True:
+        success, img = cap.read()
+        img, bboxs = detector.findFaces(img)
+        print(bboxs)
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+        cv2.putText(img, f'FPS : {int(fps)}', (20, 39), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 2)
+        cv2.imshow("Output", img)
+        cv2.waitKey(1)
+
+if __name__ == "__main__":
+    main()
